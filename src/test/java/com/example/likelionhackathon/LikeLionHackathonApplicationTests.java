fd8f02b6d99736dad0ce3d80728c06a4659ede65
jiu201325-jpg <jiu201325@gmail.com>
2026-08-13 16:38:42 +0900

    Initial commit

---
+++ src/test/java/com/example/likelionhackathon/LikeLionHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLionHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
